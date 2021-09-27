2866e42d7624f66f929ec2830ad40a83077ffb5b
Kutnyi <86615054+Kutnyi@users.noreply.github.com>
2021-09-27 19:29:43 +0300

    new changes to my homework

--- HW_1/HW_1.js
+++ HW_1/HW_1.js
@@ -1,1 +1,7 @@
-console.log("Homework_1")
+const jeep = 15.1678;
+const mazda = 123.965;
+const kia = 90.2345
+
+const maxPrice = Math.max(jeep, mazda, kia);
+const minPrice = Math.min(jeep, mazda, kia);
+const sumGoods = (jeep + mazda + kia);
